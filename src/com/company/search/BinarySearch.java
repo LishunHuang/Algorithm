73d37935ade6cf043fec4f75cf3ac368a1564286
lishun <lishun9944@gmail.com>
2022-04-21 20:20:07 -0400

    p85

---
+++ src/com/company/search/BinarySearch.java
@@ -0,0 +1,76 @@
+package com.company.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//二分查找前提必须是有序数组
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[] = {1, 8, 10, 89, 1000, 1234};
+        int index = binarySearch(arr, 0, arr.length, 1);
+        System.out.println(index);
+    }
+
+    public static int binarySearch(int[] arr, int left, int right, int findVal) {
+        //当left > right时， 说明递归整个数组，但是没有找到
+        if (left > right) return -1;
+
+        int mid = (left + right) / 2;
+        int midValue = arr[mid];
+        if (findVal > midValue) {//向右递归
+            return binarySearch(arr, mid + 1, right, findVal);
+        } else if (findVal < midValue) {
+            return binarySearch(arr, left, mid - 1, findVal);
+        } else {
+            return mid;
+        }
+    }
+
+    /**
+     * 找到所有相同的数值
+     * 在找到mid索引值，不要马上返回
+     * 向mid索引值的左边扫描，将所有满足1000的元素下标加入到集合里面
+     * 向mid索引值的右边扫描，将所有满足1000的元素下标加入到Arrayliist
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @param findVal
+     * @return
+     */
+    public static ArrayList<Integer> binarySearchAll(int[] arr, int left, int right, int findVal) {
+        //当left > right时， 说明递归整个数组，但是没有找到
+        if (left > right) return new ArrayList<>();
+
+        int mid = (left + right) / 2;
+        int midValue = arr[mid];
+        if (findVal > midValue) {//向右递归
+            return binarySearchAll(arr, mid + 1, right, findVal);
+        } else if (findVal < midValue) {
+            return binarySearchAll(arr, left, mid - 1, findVal);
+        } else {
+            ArrayList<Integer> resIndexList = new ArrayList<>();
+            int temp = mid - 1;
+            while (true) {
+                if (temp < 0 || arr[temp] != findVal) {
+                    break;
+                }
+                //否则就把temp放入到ArrayList
+                resIndexList.add(temp);
+                temp -= 1;
+            }
+            resIndexList.add(mid);
+            temp = mid + 1;
+
+            while (true) {
+                if (temp > arr.length - 1 || arr[temp] != findVal) {
+                    break;
+                }
+                //否则就把temp放入到ArrayList
+                resIndexList.add(temp);
+                temp += 1;
+            }
+            return resIndexList;
+        }
+    }
+}
---
+++ src/com/company/search/FibonacciSearch.java
@@ -0,0 +1,74 @@
+package com.company.search;
+
+import java.util.Arrays;
+
+public class FibonacciSearch {
+    public static int maxSize = 20;
+
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 10, 89, 1000, 1234};
+        System.out.println("index = " + finSearch(arr, 1));
+    }
+
+    //因为后面我们mid = low+F(k-1)-1需要使用到斐波那契数列，因此我们需要先获取到一个斐波那契数列
+    //非递归方法得到一个斐波那契数列
+    public static int[] fib() {
+        int[] f = new int[maxSize];
+        f[0] = 1;
+        f[1] = 1;
+        for (int i = 2; i < maxSize; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f;
+    }
+
+    //非递归方式
+    public static int finSearch(int[] a, int key) {
+        int low = 0;
+        int high = a.length - 1;
+        int k = 0; //表示斐波那契数列分割数值下标
+        int mid = 0;
+        int f[] = fib();
+        //获取到斐波那契分割数列的下标
+        while (high > f[k] - 1) {
+            k++;
+        }
+        //因为f[k]值可能大于a的长度，因此我们需要使用Arrays类，构造一个新的数组，并指向temp[]
+        //不足的部分会使用0填充
+        int[] temp = Arrays.copyOf(a, f[k]);
+        //实际上需求使用a数组最后的数填充temp
+        for (int i = high + 1; i < temp.length; i++) {
+            temp[i] = a[high];
+        }
+        //使用while来循环处理，来找到我们的数key
+        while (low <= high) {//只要这个条件满足，就可以一直找
+            mid = low + f[k - 1] - 1;
+            if (key < temp[mid]) { //说明向数组前面查找
+                high = mid - 1;
+                //全部元素 = 前面的元素 + 后面的元素
+                //f[k] = f[k-1] + f[k-2]
+                //因为前面有f[k-1]个元素，所以可以继续拆分f[k-1]=f[k-2]+f[k-3]
+                //即在f[k-1]的前面继续查找k--
+                //即下次循环mid = f[k-1-1]-1
+                k--;
+            } else if (key > temp[mid]) {//向后别继续查找
+                low = mid + 1;
+                //全部元素 = 前面的元素 + 后面的元素
+                //f[k] = f[k-1] + f[k-2]
+                //因为我们后面有f[k-2] 所以可以继续拆分f[k-1]=f[k-3]+f[k-4]
+                //即在f[k-2]的前面尽心查找k-=2
+                //即下次循环mid = f[k-1-2] -1
+                k -= 2;
+
+            } else {
+                if (mid <= high) {
+                    return mid;
+
+                } else {
+                    return high;
+                }
+            }
+        }
+        return -1;
+    }
+}
